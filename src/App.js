e6a605b4397541fa053393ae4dbe447f40d9b6f9
Randika Rodrigo <57132743+RandikaR21@users.noreply.github.com>
2021-07-26 17:35:48 +0530

    Initialize Project to Git

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Hello Everyone :) !!!
+        </h1>
+        <p>
+          Welcome to the live Vercel Demonstraction by - Randika Rodrigo
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
